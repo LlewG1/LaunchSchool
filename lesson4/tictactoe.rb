45ce986bb4dc68e9c2e76f563b74b4ce2630fc5d
Llewellyn Griffith <griffithl90@gmail.com>
2016-03-21 22:24:44 +0000

    Cleaned up code and bug checked

--- lesson4/tictactoe.rb
+++ lesson4/tictactoe.rb
@@ -1,15 +1,4 @@
-# Tic Tac Toe
-
-# 1. Display the initial empty 3x3 board.
-# 2. Ask the user to mark a square.
-# 3. Computer marks a square.
-# 4. Display the updated board state.
-# 5. If winner, display winner.
-# 6. If board is full, display tie.
-# 7. If neither winner nor board is full, go to #2
-# 8. Play again?
-# 9. If yes, go to #1
-# 10. Good bye!
+    # Tic Tac Toe by Llew Griffith
 
 require 'pry'
 
@@ -21,6 +10,8 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
                 [[1, 5, 9], [3, 5, 7]]
 
+    # Format methods
+
 def prompt(msg)
   puts "=> #{msg}"
 end
@@ -29,6 +20,8 @@ def joinor(array, middle=', ', word='or')
   array[-1] = "#{word} #{array.last}" if array.size > 1
   array.join(middle)
 end
+
+    # Board setup methods
 
 # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 def display_board(brd)
@@ -55,28 +48,42 @@ def initialize_board
   new_board
 end
 
-def first_turn(turn_answer, current_player)
+def alternate_player(current_player)
+    if current_player == 'Computer'
+      return 'Player'
+    elsif current_player == 'Player'
+      return 'Computer'
+    end
+end
+
+    # Initial user prompts
+
+def game_difficulty
+  prompt "Would you like to play 'easy' or dare to play 'hard'?"
+  prompt "Noobs press (e) for easy, pros press (h) for hard"
+  return gets.chomp
+end
+
+def turn_choice
+  prompt "Would you like to go first or second?"
+  prompt "Press (f) for sissy first turn or (s) to play like a boss and let computer go first"
+  turn_answer = gets.chomp
+  
   if turn_answer.downcase.start_with?('f')
-    current_player[:turn] += 1
+    return 'Player'
   elsif turn_answer.downcase.start_with?('s')
-    current_player[:turn] += 2
+    return 'Computer'
   end
 end
 
-def alternate_player(current_player)
-    current_player[:turn] += 1
-end
+    # Gameplay methods
 
 def place_piece!(board, current_player, difficulty_answer)
-  if current_player[:turn].odd?
+  if current_player == 'Computer'
     computer_places_piece!(board, difficulty_answer)
-  else
+  elsif current_player == 'Player'
     player_places_piece!(board)
   end
-end
-
-def empty_squares(brd)
-  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece!(brd)
@@ -96,6 +103,10 @@ def find_at_risk_square(line, board, marker)
   else
     nil
   end
+end
+
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def computer_places_piece!(brd, difficulty_answer)
@@ -125,6 +136,8 @@ def computer_places_piece!(brd, difficulty_answer)
     brd[square] = COMPUTER_MARKER
 end
 
+    # Game end detection methods
+
 def board_full?(brd)
   empty_squares(brd).empty?
 end
@@ -144,59 +157,103 @@ def detect_winner(brd)
   nil
 end
 
-player_wins = 0
-computer_wins = 0
-ties = 0
-# score = { player_wins: 0, computer_wins: 0, ties: 0 }
-current_player = {turn: 0}
+    # Post game messages and score tracking 
 
-prompt "Would you like to play 'easy' or dare to play 'hard'?"
-prompt "Press (e) for easy and (h) for hard"
-difficulty_answer = gets.chomp
-
-prompt "Would you like to go first or second?"
-prompt "Press (f) for first turn or (s) to let computer go first and take second turn"
-turn_answer = gets.chomp
-
-first_turn(turn_answer, current_player)
-
-loop do
-  board = initialize_board
-  
-  display_board(board)
-  binding.pry
-  place_piece!(board, current_player, difficulty_answer)
-  current_player = alternate_player(current_player)
-
+def winning_messages(board)
   if someone_won?(board)
+    display_board(board)
     prompt "#{detect_winner(board)} won!"
   elsif board_full?(board)
+    display_board(board)
     prompt "It's a tie"
   end
-
-  if detect_winner(board) == 'Player'
-    player_wins += 1
-  elsif detect_winner(board) == 'Computer'
-    computer_wins += 1
-  else
-    ties += 1
-  end
-  
-  prompt "Player has #{player_wins} wins, Computer has #{computer_wins} wins, there have been #{ties} ties."
-  
-  if player_wins == 5
-    prompt "Player has taken the best of 5 win!"
-    break
-  elsif computer_wins == 5
-    prompt "Computer has taken the best of 5 win!"
-    break
-  end
-  
-  # game_number = player_wins + computer_wins + ties + 1
-  # prompt "Ready for game number #{game_number}? Hit any key to continue or (n) to forfeit!"
-  # answer = gets.chomp
-  # break if answer.downcase.start_with?('n')
   
 end
 
-prompt "Thankyou for playing Tic Tac Toe!"
+def display_game_score(board, score) 
+  if board_full?(board) || someone_won?(board)
+    prompt "Player has #{score[:player_wins]} wins, Computer has #{score[:computer_wins]} wins, there have been #{score[:ties]} ties."
+  end
+end
+
+def win_conditions(score)
+  if score[:player_wins] == 5
+    prompt "Player has taken the best of 5 win!"
+    return true
+  elsif score[:computer_wins] == 5
+    prompt "Computer has taken the best of 5 win!"
+    return true
+  end
+end
+
+def score_tracking(score, board)
+  if detect_winner(board) == 'Player'
+    score[:player_wins] += 1
+  elsif detect_winner(board) == 'Computer'
+    score[:computer_wins] += 1
+  elsif board_full?(board)
+    score[:ties] += 1
+  end 
+end
+
+def continue_playing(board, score)
+  if detect_winner(board) || board_full?(board)
+    game_number = score[:player_wins] + score[:computer_wins] + score[:ties] + 1
+    prompt "Ready for game number #{game_number}? Hit any key to continue or (n) to forfeit!"
+    answer = gets.chomp
+    true if answer.downcase.start_with?('n')
+  end  
+end
+
+    # Reset methods
+
+def reset_player(current_player, first_turn, board)
+  if board_full?(board) || someone_won?(board)
+    return first_turn
+  else
+    return current_player
+  end
+end
+
+def reset_board(board)
+  if board_full?(board) || someone_won?(board)
+    return initialize_board
+  else
+    return board
+  end
+end
+
+    # Program
+
+score = { player_wins: 0, computer_wins: 0, ties: 0 } 
+
+difficulty_answer = game_difficulty
+first_turn = turn_choice
+
+current_player = first_turn
+board = initialize_board
+
+loop do
+ 
+  display_board(board)
+  
+  # Gameplay
+  place_piece!(board, current_player, difficulty_answer)
+  current_player = alternate_player(current_player)
+  
+  # Post game messages and score tracking
+  winning_messages(board)
+  score_tracking(score, board)  
+  display_game_score(board, score)
+
+  # End game if forfeit or 5 wins
+  break if win_conditions(score)
+  break if continue_playing(board, score)
+  
+  # Reset gameboard
+  current_player = reset_player(current_player, first_turn, board)
+  board = reset_board(board)
+
+end
+
+prompt "Thanks for playing Tic Tac Toe!"
